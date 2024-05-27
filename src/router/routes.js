e1357e3ad1dcd4a329b85327c77f3ccfd25fb27d
Kevin Machava <kevinmachava20@gmail.com>
2024-05-27 14:26:21 +0200

    save

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,14 +3,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/landing.vue') },
-      { path: '/home', component: () => import('pages/IndexPage.vue') },
-      { path: '/about', component: () => import('pages/about.vue') },
-      { path: '/help', component: () => import('pages/help.vue') },
-      { path: '/settings', component: () => import('pages/settings.vue') },
-      { path: '/chat/:id', component: () => import('pages/ChatPage.vue') },
-      { path: '/chatlist', component: () => import('pages/ChatListPage.vue') },
-      { path: '/commands', component: () => import('pages/CustomCommands.vue') },
+      { path: '', component: () => import('pages/landing.vue'), name: 'landing' },
+      { path: '/home', component: () => import('pages/IndexPage.vue'), name: 'home' },
+      { path: '/about', component: () => import('pages/about.vue'), name: 'about' },
+      { path: '/help', component: () => import('pages/help.vue'), name: 'help' },
+      { path: '/settings', component: () => import('pages/settings.vue'), name: 'settings' },
+      { path: '/chat/:id', component: () => import('pages/ChatPage.vue'), name: 'chat' },
+      { path: '/chatlist', component: () => import('pages/ChatListPage.vue'), name: 'chatlist' },
+      { path: '/commands', component: () => import('pages/CustomCommands.vue'), name: 'commands' },
     ]
   },
 
