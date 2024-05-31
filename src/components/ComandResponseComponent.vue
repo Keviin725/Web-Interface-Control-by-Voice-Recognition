548db2debf90023a3f5f6793c894fff5f023fa3b
Kevin Machava <kevinmachava20@gmail.com>
2024-05-31 10:40:41 +0200

    Create ComandResponseComponent.vue

---
+++ src/components/ComandResponseComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-card v-if="commandResponse" class="command-response">
+    <q-card-section>
+      <div v-if="commandResponse.type === 'weather'">
+        <p>{{ commandResponse.data }}</p>
+      </div>
+      <div v-if="commandResponse.type === 'camera'">
+        <p>{{ commandResponse.data }}</p>
+      </div>
+      <!-- Adicione outros tipos de respostas conforme necessário -->
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+import { useCommandStore } from '../store';
+
+export default {
+  setup() {
+    const commandStore = useCommandStore();
+    return {
+      commandResponse: commandStore.commandResponse,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.command-response {
+  margin: 1rem 0;
+}
+</style>
